a8f75bc0018da1377fa0473adcd591f5690c1ca1
taisa <aikou@hpc.is.uec.ac.jp>
2017-05-14 14:34:58 +0000

    [MOD] filter was installed

--- filter.c
+++ filter.c
@@ -18,22 +18,48 @@
 void printBlackList()
 {
 	black_list_t * tmp = blackuser;
+	int i = 0;
+
 	while ( tmp != NULL )
 	{
-		fprintf( stdout, "userip:%s flow_number:%d\n", tmp->userip, tmp->flow_number );
+		fprintf( stdout, "[NO%03d] -- userip:%s flow_number:%d\n", i, tmp->userip, tmp->flow_number );
 		printSentFlow( tmp );
 		tmp = tmp->next;
+		i++;
 	}
 }
 
 void printSentFlow( black_list_t * user_node )
 {
 	sent_flow_t * tmp_sent_node;
+	int i = 1;
 	tmp_sent_node = user_node->blacksentflow;
 	while ( tmp_sent_node != NULL )
 	{
-		fprintf( stdout, "		%s %s %d %d, %d\n", tmp_sent_node->flowid.dstip, tmp_sent_node->flowid.protcol, tmp_sent_node->flowid.dstport, tmp_sent_node->flowid.srcport, tmp_sent_node->count  );
+		fprintf( stdout, "	|	[NO%03d] -- %s %s %d %d, %d\n", i, tmp_sent_node->flowid.dstip, tmp_sent_node->flowid.protcol, tmp_sent_node->flowid.dstport, tmp_sent_node->flowid.srcport, tmp_sent_node->count  );
 		tmp_sent_node = tmp_sent_node->next;
+		i++;
+	}
+}
+
+void printRegisteredBlackList()
+{
+	black_list_t * tmp = blackuser;
+	int i = 0;
+
+	while( tmp != NULL )
+	{
+		if ( tmp->flow_number == 0 )
+		{
+			break;
+		}
+		else
+		{
+			fprintf( stdout, "[NO%03d] -- userip:%s flow_number:%d\n",i, tmp->userip, tmp->flow_number );
+			printSentFlow( tmp );
+		}
+		i++;
+		tmp = tmp->next;
 	}
 }
 
@@ -46,26 +72,18 @@ int makeBlackList()
 
 	blackuser = malloc( sizeof( black_list_t ) );
 	initializeBlackUserList( blackuser );
-
 	tmp = blackuser;
 	tmp->prev = NULL;
 
 	for ( i = 0 ; i < BLACKUSER_MAX - 1 ; i = i + 1 )
 	{
-		// user$B$N(BIP$B%"%I%l%9$r=i4|2=(B
-		// strcpy( tmp->userip, "0" );
 		tmp->next = malloc( sizeof( black_list_t ) );
 		initializeBlackUserList( tmp->next );
-		tmp1 = tmp;
+		tmp->next->prev = tmp;
 		tmp = tmp->next;
-		tmp->prev = tmp1;
 	}
 
-	// $B:G8e$N%N!<%I$O=i4|2=$5$l$J$$$?$a(B
-	blackuser_end = tmp;
-	tmp->next = NULL;
-
-	makeFlowList();
+	blackuser_end = tmp->prev;
 	return 1;
 }
 
@@ -86,6 +104,8 @@ void initializeBlackUserList( black_list_t * user_node  )
 void initializeFlowList( sent_flow_t * flow_node )
 {
 	flow_node->count = 0;
+	flow_node->next = NULL;
+	flow_node->prev = NULL;
 	strcpy( flow_node->flowid.srcip, "0" );
 	strcpy( flow_node->flowid.dstip, "0" );
 	flow_node->flowid.srcport = 0;
@@ -103,7 +123,7 @@ sent_flow_t * addFlow( black_list_t * user_node )
 
 	user_node->flow_number = user_node->flow_number + 1;
 	tmp = user_node->blacksentflow;
-	fprintf( stdout, "blacksetnflow address is %p\n", tmp );
+
 	while ( tmp != NULL )
 	{	// 最後のノードまで移動する
 		last = tmp;
@@ -111,6 +131,7 @@ sent_flow_t * addFlow( black_list_t * user_node )
 	}
 
 	tmp = malloc( sizeof( sent_flow_t ) );
+	tmp->prev = last;
 	last->next = tmp;
 	tmp->next = NULL;
 	return tmp;
@@ -138,10 +159,16 @@ int makeFlowList()
 	return 1;
 }
 
+//////////////////////////////
+/* ブラックリストの初期化を行う */
+/////////////////////////////
 void blackListInit()
 {
 	black_list_t * tmp;
+	sent_flow_t * tmp_node;
+	sent_flow_t * tmp1;
 	int i = 0;
+
 	tmp = blackuser;
 	while ( tmp != NULL )
 	{
@@ -154,21 +181,6 @@ void blackListInit()
 		tmp = tmp->next; 
 	}
 }
-
-//void printBlackList()
-//{
-//	black_list_t * tmp_node;
-//	int i = 0, j = 0;
-//
-//	tmp_node = blackuser;
-//	while ( tmp_node != NULL )
-//	{
-//		fprintf( stdout, "NO:%d, userip:%s flownumber:%d\n", i, tmp_node->userip, tmp_node->flow_number );
-//		//fprintf( stdout, "NO%d:%s %s %s %d %d ", i, tmp_node->flowid.srcip, tmp_node->flowid.dstip, tmp_node->flowid.protcol, tmp_node->flowid.srcport, tmp_node->flowid.dstport ); 
-//		tmp_node = tmp_node->next;
-//		i = i + 1;
-//	}
-//}
 
 ////////////////////////////////////////////////////////////////
 /* $B%V%i%C%/%j%9%HFb$N8!:w(B, $BEPO?$d:o=|$NA`:n$r9T$&4X?t$NBgK\(B   */
@@ -245,7 +257,7 @@ int substituteUser( black_list_t * tmp, tuple_t tuple )
 
 int substituteFlow( sent_flow_t * flow_node, tuple_t tuple  )
 {
-	flow_node->count = flow_node->count + 1;
+	flow_node->count = 1;
 	strcpy( flow_node->flowid.srcip, tuple.srcip );
 	strcpy( flow_node->flowid.dstip, tuple.dstip );
 	flow_node->flowid.srcport = tuple.srcport;
@@ -312,60 +324,33 @@ int removeFlow( sent_flow_t * remove_node, black_list_t * user_node )
 		//removeUser( user_node );
 	}
 }
+
 int deleteFlow( sent_flow_t * flow_node )
 {
 	sent_flow_t * tmp;
 	sent_flow_t * tmp1;
+
 	int i = 0;
 	tmp = flow_node;
 
 	while ( tmp != NULL )
 	{
-		fprintf( stdout, "%d\n", i );
-		tmp1 = tmp->next;
-		free( tmp );
-		tmp = tmp1;
-		i++;
+		tmp = deleteLastFlowNode( tmp );
 	}
 
 	return i;
 }
-//////////////////////////////////////////////////
-/* $B%V%i%C%/%j%9%H$KEPO?$5$l$?(Buser$B$r:o=|$9$k4X?t(B */
-//////////////////////////////////////////////////
-//void removeUser( black_list_t * user_node )
-//{
-	//blackListSwap( user_node );
-//}
 
-//////////////////////////////////////////////
-/* $B%V%i%C%/%j%9%H$N%(%s%H%j$rF~$lBX$($k4X?t(B */
-//////////////////////////////////////////////
-//void blackListSwap( black_list_t * user_node )
-//{
-//	black_list_t * tmp;
-//	if ( user_node == blackuser )
-//	{
-//		// $B%V%i%C%/%j%9%H$N:G=i$NMWAG$@$C$?>l9g(B
-//		tmp = blackuser;
-//		blackuser = blackuser->next;
-//		blackuser->prev = NULL;
-//
-//		tmp->prev = blackuser_end;
-//		blackuser_end = tmp;
-//		tmp->next = NULL;
-//	}
-//	else if ( user_node == blackuser_end )
-//	{
-//		// $B%V%i%C%/%j%9%H$N:G8e$NMWAG$N>l9g(B
-//		// $B:G8e$NMWAG$O=i4|2=$r9T$&(B( )
-//		initializeFlowList( user_node->blacksentflow );
-//		initializeBlackUserList( user_node );
-//	}
-//	else
-//	{
-//		tmp = user_node;
-////		user_node;
-//	}
-//}
-//
+sent_flow_t * deleteLastFlowNode( sent_flow_t * flow_node )
+{
+	sent_flow_t * tmp;
+	if ( tmp == NULL )
+	{
+		return NULL;
+	}
+	tmp = flow_node->next;
+	free( flow_node );
+	flow_node = NULL;
+
+	return tmp;
+}
